8334c1c4da8bb208113ecd09c2b651ac8379274a
Lekta23 <48383181+Lekta23@users.noreply.github.com>
2023-10-13 15:59:56 +0200

    BACK : TP1

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.status(200);
-    res.send("Welcome to root URL of Server");
+    res.send("Hello World");
 });
 
 app.listen(PORT, (error) => {
